601e55999641693230da63f3e3eae055164c1cc0
ashmit1795 <ashmitpatra77@gmail.com>
2026-06-29 23:59:07 +0530

    feat: update indexing plan to indicate vector embeddings generation is complete and add vectorisation module for document storage

--- module07/index.js
+++ module07/index.js
@@ -3,7 +3,7 @@
  * Stage 1: Indexing
  * 1. Load the document - pdf, txt ✅
  * 2. Chunk the document into smaller pieces ✅
- * 3. Generate vector embeddings for each chunk
+ * 3. Generate vector embeddings for each chunk ✅
  *
  * Stage 2: Querying
  * 1. Setup LLM
---
+++ module07/vectorisation.js
@@ -0,0 +1,32 @@
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+import dotenv from "dotenv";
+import { OpenAIEmbeddings } from "@langchain/openai";
+
+dotenv.config();
+
+
+const embeddings = new OpenAIEmbeddings({
+	model: "text-embedding-3-small",
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const pinecone = new PineconeClient({
+	apiKey: process.env.PINECONE_API_KEY,
+});
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+	pineconeIndex,
+	maxConcurrency: 5,
+});
+
+export async function addDocumentsToVectorStore(documents) {
+	try {
+		return await vectorStore.addDocuments(documents);
+	} catch (error) {
+		console.error("Error adding documents to vector store:", error);
+		throw error;
+	}
+}
